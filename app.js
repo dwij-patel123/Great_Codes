977b4ea7b00433d4326f91b7925d1d065871114e
Dwij Patel <dwijpatel15@gmail.com>
2023-02-16 22:26:19 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.splice(2,0,"watermelon","jackfruit");
-console.log(fruits);
+let firstnum = 10;
+let secondnum = 11;
+let displayEL = Document.getElementById("display-el");
+let cardsEL = Document.getElementById("cards-el");
+let sumEL = Document.getElementById("sum-el");
+let sum = firstnum + secondnum;
+let message = "";
+console.log(displayEL);
+
+function myfunction(){
+    if(sum < 21){
+        message = "do you want to pick another card";
+        
+    }else if(sum === 21){
+        message = "you got blackjack!";
+    
+    }else{
+        message = "you are out of game";
+    }
+    
+    displayEL.textContent = message;
+}
+
